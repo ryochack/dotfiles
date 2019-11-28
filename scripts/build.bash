30f10055f83878096c18a75ab1222c423ad00f18
ryochack <ryochack@gmail.com>
2019-11-28 09:10:44 +0900

    scripts: update build.bash

--- scripts/build.bash
+++ scripts/build.bash
@@ -7,16 +7,19 @@ if [ $# -eq 0 ]; then
 fi
 
 cd $(dirname $0)
-script_dir=$PWD
-prefix=$HOME/.local
+SCRIPT_DIR=$PWD
+PREFIX=$HOME/.local
 
-targets=("vim" "go" "tmux" "gdb-dashboard" "opencv" "ctags" "pdfgrep" "rustinstant" "peep" "librealsense")
+PROCESSORS="$(grep processor /proc/cpuinfo | wc -l)"
+MAKE_JOBS_OPTION="-j${PROCESSORS}"
+
+TARGETS=("vim" "go" "tmux" "gdb-dashboard" "opencv" "ctags" "pdfgrep" "rustinstant" "peep" "librealsense" "halide")
 
 build_vim() {
 	echo "build vim..."
 	cd vim/
 	make distclean
-	./configure --prefix="${prefix}" \
+	./configure --prefix="${PREFIX}" \
 		--disable-darwin \
 		--disable-smack \
 		--disable-selinux \
@@ -24,7 +27,7 @@ build_vim() {
 		--enable-luainterp=yes \
 		--enable-mzschemeinterp \
 		--enable-perlinterp=yes \
-		--enable-pythoninterp=yes \
+		--enable-pythoninterp=no \
 		--enable-python3interp=yes \
 		--enable-tclinterp=yes \
 		--enable-rubyinterp=yes \
@@ -34,22 +37,22 @@ build_vim() {
 		--enable-fontset \
 		--enable-gui=no \
 		--enable-terminal \
-		--with-local-dir="${prefix}" \
+		--with-local-dir="${PREFIX}" \
 		--with-features=huge \
 		--with-lua-prefix=/usr \
 		--with-ruby-command=ruby \
 		cd src
-	make
+	make "${MAKE_JOBS_OPTION}"
 	make install
-	cd "${script_dir}"
+	cd "${SCRIPT_DIR}"
 }
 
 build_go() {
 	echo "build go..."
-	export GOROOT_BOOTSTRAP=${script_dir}/go1.4-bootstrap
+	export GOROOT_BOOTSTRAP=${SCRIPT_DIR}/go1.4-bootstrap
 	cd go/src
 	./all.bash
-	cd "${script_dir}"
+	cd "${SCRIPT_DIR}"
 }
 
 build_gdb-dashboard() {
@@ -57,7 +60,7 @@ build_gdb-dashboard() {
 	cp gdb-dashboard/.gdbinit ~/
 	# for embedded-rust. refs: http://blog.japaric.io/quickstart/
 	echo 'set auto-load safe-path /' >> ~/.gdbinit
-	cd "${script_dir}"
+	cd "${SCRIPT_DIR}"
 }
 
 build_opencv() {
@@ -65,45 +68,45 @@ build_opencv() {
 	cd opencv/opencv/
 	install -d _build && cd _build
 	cmake -D CMAKE_BUILD_TYPE=RELEASE \
-		-D CMAKE_INSTALL_PREFIX="${prefix}" \
+		-D CMAKE_INSTALL_PREFIX="${PREFIX}" \
 		-D INSTALL_PYTHON_EXAMPLES=ON \
 		-D OPENCV_EXTRA_MODULES_PATH=../../contrib/modules \
 		-D BUILD_opencv_python3=ON \
 		-D BUILD_EXAMPLES=ON ..
-	make -j2
+	make "${MAKE_JOBS_OPTION}"
 	make install
 	sudo ldconfig
-	cd "${script_dir}"
+	cd "${SCRIPT_DIR}"
 }
 
 build_ctags() {
 	echo "build universal-ctags..."
 	cd ctags/
 	./autogen.sh
-	./configure --prefix="${prefix}"
+	./configure --prefix="${PREFIX}"
 	make
 	make install
-	cd "${script_dir}"
+	cd "${SCRIPT_DIR}"
 }
 
 build_pdfgrep() {
 	echo "build pdfgrep..."
-	ln -fs "${script_dir}"/pdfgrep/pdfgrep.sh "${prefix}"/bin/pdfgrep
-	chmod a+x "${prefix}"/bin/pdfgrep
+	ln -fs "${SCRIPT_DIR}"/pdfgrep/pdfgrep.sh "${PREFIX}"/bin/pdfgrep
+	chmod a+x "${PREFIX}"/bin/pdfgrep
 }
 
 build_rustinstant() {
 	echo "build rustinstant..."
-	ln -fs "${script_dir}"/rustinstant/rustinstant.sh "${prefix}"/bin/rustinstant
-	chmod a+x "${prefix}"/bin/rustinstant
+	ln -fs "${SCRIPT_DIR}"/rustinstant/rustinstant.sh "${PREFIX}"/bin/rustinstant
+	chmod a+x "${PREFIX}"/bin/rustinstant
 }
 
 build_peep() {
 	echo "build peep..."
 	cd peep/
 	cargo build --release
-	ln -fs "${script_dir}"/peep/target/release/peep "${prefix}"/bin/peep
-	cd "${script_dir}"
+	ln -fs "${SCRIPT_DIR}"/peep/target/release/peep "${PREFIX}"/bin/peep
+	cd "${SCRIPT_DIR}"
 }
 
 build_librealsense() {
@@ -111,22 +114,37 @@ build_librealsense() {
 	cd librealsense/
 	install -d _build && cd _build
 	cmake .. \
-        -D BUILT_EXAMPLES=ture \
-        -D CMAKE_BUILD_TYPE=Release \
-		-D CMAKE_INSTALL_PREFIX="${prefix}"
-    make uninstall
-    make clean
-    make -j8
-    make install
-	cd "${script_dir}"
+		-D BUILT_EXAMPLES=ture \
+		-D CMAKE_BUILD_TYPE=Release \
+		-D CMAKE_INSTALL_PREFIX="${PREFIX}"
+	make uninstall
+	make clean
+	make "${MAKE_JOBS_OPTION}"
+	make install
+	cd "${SCRIPT_DIR}"
 }
 
-for argv in "$@"; do
-    for target in "${targets[@]}"; do
-        if [ "${argv%/}" == "${target}" ]; then
-            eval "build_${target}"
-            break
-        fi
-    done
+build_halide() {
+	echo "build halide..."
+	cd halide/
+	install -d _build && cd _build
+	cmake .. \
+		-D LLVM_DIR=/usr/lib/cmake/llvm \
+        -D CMAKE_EXPORT_COMPILE_COMMANDS=ON \
+		-D CMAKE_INSTALL_PREFIX="${PREFIX}"
+	#make uninstall
+	make clean
+	make "${MAKE_JOBS_OPTION}"
+	#make install
+	cd "${SCRIPT_DIR}"
+}
+
+for ARGV in "$@"; do
+	for TARGET in "${TARGETS[@]}"; do
+		if [ "${ARGV%/}" == "${TARGET}" ]; then
+			eval "build_${TARGET}"
+			break
+		fi
+	done
 done
 
